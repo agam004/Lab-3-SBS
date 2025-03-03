61a5e793e1e42d06b08ecb9e7e84ea5f47981d3b
agam004 <singhguragampreet@gmail.com>
2025-03-03 11:58:32 -0500

    Add number classification and looping functionality to test.py

--- test.py
+++ test.py
@@ -1,1 +1,19 @@
-print("Hello World")
+number = int(input("Enter a number: "))
+if number ==0:
+    print("The number is zero")
+elif number > 0:
+    print("The number is positive")
+else:
+    print("The number is negative")
+
+
+userOpinoin = input("Want to run loop? (yes/no): ")
+startingpoint = 0
+endingpoint = 20
+
+if userOpinoin == "yes" | userOpinoin == "Yes"| userOpinoin == "YES" | userOpinoin == "y" | userOpinoin == "Y":
+    while startingpoint <= endingpoint:
+        print(startingpoint)
+        startingpoint += 1 
+else:
+    print("Thank you for using the program")
